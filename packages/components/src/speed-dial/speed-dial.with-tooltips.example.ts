10239dbd79ae928078d3b926e3026400e485b5b9
NAJIH Driss <najih.driss73@gmail.com>
2026-05-08 05:09:39 +0200

    fix(examples): unblock build for menubar+speed-dial examples (stub menubar submenu pending API)

--- packages/components/src/speed-dial/speed-dial.with-tooltips.example.ts
+++ packages/components/src/speed-dial/speed-dial.with-tooltips.example.ts
@@ -5,17 +5,13 @@ import {
   KjSpeedDialComponent,
   KjSpeedDialTriggerComponent,
 } from './speed-dial';
-import {
-  KjTooltipComponent,
-  KjTooltipContentComponent,
-} from '../tooltip/tooltip';
+import { KjTooltipTrigger, KjTooltipContent } from '../tooltip/tooltip';
 
 /**
  * Speed Dial where each action button has a tooltip describing the action.
  *
- * Tooltips are composed per-action via `<kj-tooltip [kjTooltipTriggerFor]>`
- * around the action button. The action's `kjAriaLabel` is the AT-visible
- * name; the tooltip is purely a sighted-user supplement.
+ * The action's `kjAriaLabel` is the AT-visible name; the tooltip is purely a
+ * sighted-user supplement.
  */
 @Component({
   selector: 'kj-speed-dial-with-tooltips-example',
@@ -25,8 +21,8 @@ import {
     KjSpeedDialTriggerComponent,
     KjSpeedDialActionsComponent,
     KjSpeedDialActionComponent,
-    KjTooltipComponent,
-    KjTooltipContentComponent,
+    KjTooltipTrigger,
+    KjTooltipContent,
   ],
   styles: [`
     :host {
@@ -42,26 +38,14 @@ import {
     <kj-speed-dial kjDirection="up" kjPosition="static">
       <kj-speed-dial-trigger kjAriaLabel="Open quick actions">+</kj-speed-dial-trigger>
       <kj-speed-dial-actions>
-        <kj-tooltip [kjTooltipTriggerFor]="editTip" kjTooltipSide="left">
-          <kj-speed-dial-action kjAriaLabel="Edit">E</kj-speed-dial-action>
-        </kj-tooltip>
-        <ng-template #editTip>
-          <kj-tooltip-content>Edit document</kj-tooltip-content>
-        </ng-template>
+        <kj-speed-dial-action kjTooltipTrigger #editT="kjTooltipTrigger" kjAriaLabel="Edit">E</kj-speed-dial-action>
+        <kj-tooltip-content [kjFor]="editT" kjSide="left">Edit document</kj-tooltip-content>
 
-        <kj-tooltip [kjTooltipTriggerFor]="shareTip" kjTooltipSide="left">
-          <kj-speed-dial-action kjAriaLabel="Share">S</kj-speed-dial-action>
-        </kj-tooltip>
-        <ng-template #shareTip>
-          <kj-tooltip-content>Share with team</kj-tooltip-content>
-        </ng-template>
+        <kj-speed-dial-action kjTooltipTrigger #shareT="kjTooltipTrigger" kjAriaLabel="Share">S</kj-speed-dial-action>
+        <kj-tooltip-content [kjFor]="shareT" kjSide="left">Share with team</kj-tooltip-content>
 
-        <kj-tooltip [kjTooltipTriggerFor]="deleteTip" kjTooltipSide="left">
-          <kj-speed-dial-action kjAriaLabel="Delete" kjVariant="destructive">D</kj-speed-dial-action>
-        </kj-tooltip>
-        <ng-template #deleteTip>
-          <kj-tooltip-content>Move to trash</kj-tooltip-content>
-        </ng-template>
+        <kj-speed-dial-action kjTooltipTrigger #deleteT="kjTooltipTrigger" kjAriaLabel="Delete" kjVariant="destructive">D</kj-speed-dial-action>
+        <kj-tooltip-content [kjFor]="deleteT" kjSide="left">Move to trash</kj-tooltip-content>
       </kj-speed-dial-actions>
     </kj-speed-dial>
   `,
