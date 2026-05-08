37879257ddcb92ee9da6a37a79b9cdc4cabf8f38
NAJIH Driss <najih.driss73@gmail.com>
2026-05-08 03:05:12 +0200

    feat(popover): migrate onto overlay primitives; absorb hovercard via kjTrigger='hover'

--- packages/core/src/popover/index.ts
+++ packages/core/src/popover/index.ts
@@ -1,20 +1,5 @@
-export { KjPopover } from './popover';
-export { KjPopoverTrigger } from './popover-trigger';
-export { KjPopoverContent } from './popover-content';
-export { KjPopoverArrow } from './popover-arrow';
-export { KjPopoverTitle } from './popover-title';
-export { KjPopoverClose } from './popover-close';
-
-export {
-  KJ_POPOVER,
-  KJ_POPOVER_DEFAULTS,
-  KJ_POPOVER_BUILTIN_DEFAULTS,
-  type KjPopoverContext,
-  type KjPopoverDefaults,
-  type KjPopoverSide,
-  type KjPopoverAlign,
-  type KjPopoverTriggerEvent,
-  type KjPopoverCloseEvent,
-  type KjPopoverCloseReason,
-  type KjAutoFocusEvent,
-} from './popover.context';
+export * from './popover-trigger';
+export * from './popover-content';
+export * from './popover-arrow';
+export * from './popover-close';
+export * from './popover-title';
